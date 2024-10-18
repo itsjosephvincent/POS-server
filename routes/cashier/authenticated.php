caaf5fd244097abed35eafb1e8d71c22f12a1e08
Joseph <josephvincentlimbaroc@gmail.com>
2024-10-18 08:38:54 +0800

    updated cashier authenticated routes

--- routes/cashier/authenticated.php
+++ routes/cashier/authenticated.php
@@ -1,6 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\Cashier\AuthController;
+use App\Http\Controllers\Api\Cashier\CategoryController;
+use App\Http\Controllers\Api\Cashier\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/auth/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResources([
+        'categories' => CategoryController::class,
+        'products' => ProductController::class,
+    ]);
+
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+});
