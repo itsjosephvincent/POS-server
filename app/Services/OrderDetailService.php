0e6dc4d3d468155eeade47f5a9fd5558a7434e34
Joseph <josephvincentlimbaroc@gmail.com>
2024-10-19 06:49:12 +0800

    updated order detail service

--- app/Services/OrderDetailService.php
+++ app/Services/OrderDetailService.php
@@ -2,11 +2,15 @@
 
 namespace App\Services;
 
+use App\Http\Resources\OrderDetailResource;
 use App\Interfaces\Repositories\OrderDetailRepositoryInterface;
 use App\Interfaces\Services\OrderDetailServiceInterface;
+use App\Traits\SortingTraits;
 
 class OrderDetailService implements OrderDetailServiceInterface
 {
+    use SortingTraits;
+
     private $orderDetailRepository;
 
     public function __construct(OrderDetailRepositoryInterface $orderDetailRepository)
@@ -16,11 +20,18 @@ class OrderDetailService implements OrderDetailServiceInterface
 
     public function findOrderDetails(object $payload)
     {
-        //
+        $sortField = $this->sortField($payload, 'created_at');
+        $sortOrder = $this->sortOrder($payload, 'asc');
+
+        $details = $this->orderDetailRepository->findMany($payload, $sortField, $sortOrder);
+
+        return OrderDetailResource::collection($details);
     }
 
     public function findOrderDetail(string $uuid)
     {
-        //
+        $detail = $this->orderDetailRepository->findByUuid($uuid);
+
+        return new OrderDetailResource($detail);
     }
 }
