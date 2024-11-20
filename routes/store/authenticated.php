953d281a21b30f401f3347db6345c452e2c34c32
Nick Lobaton <nickm3gtr@gmail.com>
2024-11-20 16:33:50 +0800

    stores report

--- routes/store/authenticated.php
+++ routes/store/authenticated.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\Store\AuthController;
 use App\Http\Controllers\Api\Store\CashierController;
 use App\Http\Controllers\Api\Store\TableController;
+use App\Http\Controllers\Api\Store\ReportController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -10,6 +11,13 @@ Route::middleware('auth:sanctum')->group(function () {
         'cashiers' => CashierController::class,
         'tables' => TableController::class,
     ]);
+
+    Route::prefix('reports')->group(function () {
+        Route::get('/summary', [ReportController::class, 'summary']);
+        Route::get('/popular_items', [ReportController::class, 'popular_items']);
+        Route::get('/category_earnings', [ReportController::class, 'category_earnings']);
+        Route::get('/store_earnings', [ReportController::class, 'store_earnings']);
+    });
 
     Route::post('/auth/logout', [AuthController::class, 'logout']);
 });
